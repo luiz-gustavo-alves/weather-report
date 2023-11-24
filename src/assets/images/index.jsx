1086243822fe4d5570aab2f0d8e65e6cf6f8420a
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-11-24 19:01:13 -0300

    assets: add weather icons

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,4 +1,43 @@
 import coat from './coat.jpg';
 import searchIcon from './searchIcon.svg';
+import icon_01d from './icon_01d.png';
+import icon_01n from './icon_01n.png';
+import icon_02d from './icon_02d.png';
+import icon_02n from './icon_02n.png';
+import icon_03d from './icon_03d.png';
+import icon_03n from './icon_03n.png';
+import icon_04d from './icon_04d.png';
+import icon_04n from './icon_04n.png';
+import icon_09d from './icon_09d.png';
+import icon_09n from './icon_09n.png';
+import icon_10d from './icon_10d.png';
+import icon_10n from './icon_10n.png';
+import icon_11d from './icon_11d.png';
+import icon_11n from './icon_11n.png';
+import icon_13d from './icon_13d.png';
+import icon_13n from './icon_13n.png';
+import icon_50d from './icon_50d.png';
+import icon_50n from './icon_50n.png';
 
-export { coat, searchIcon };
+export {
+  coat,
+  searchIcon,
+  icon_01d,
+  icon_01n,
+  icon_02d,
+  icon_02n,
+  icon_03d,
+  icon_03n,
+  icon_04d,
+  icon_04n,
+  icon_09d,
+  icon_09n,
+  icon_10d,
+  icon_10n,
+  icon_11d,
+  icon_11n,
+  icon_13d,
+  icon_13n,
+  icon_50d,
+  icon_50n,
+};
