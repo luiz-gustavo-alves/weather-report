83d8316c1eb8f7ead87cf57f3ada41fdb2df42d3
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-11-24 20:16:44 -0300

    feat(service): update getCurrentWeatherByCity data return

--- src/services/OpenWeather/weatherApi.js
+++ src/services/OpenWeather/weatherApi.js
@@ -1,8 +1,31 @@
 import api, { API_KEYS } from './api';
+import { formatFloatPrecision } from '../../utils/format-utils';
 
 async function getCurrentWeatherByCity(city, unit) {
   const unitType = unit === 'celsius' ? 'metric' : 'imperial';
-  return await api.get(`/weather?q=${city}&appid=${API_KEYS.OPENWEATHER}&units=${unitType}`);
+  const response = await api.get(`/weather?q=${city}&appid=${API_KEYS.OPENWEATHER}&units=${unitType}`);
+
+  const { data } = response;
+  const weatherData = {
+    name: data.name,
+    condition: data.weather[0].main,
+    icon: data.weather[0].icon,
+    coord: {
+      lat: formatFloatPrecision(data.coord.lat, 2),
+      lon: formatFloatPrecision(data.coord.lon, 2),
+    },
+    temp: {
+      main: formatFloatPrecision(data.main.temp),
+      min: formatFloatPrecision(data.main.temp_min),
+      max: formatFloatPrecision(data.main.temp_max),
+    },
+    details: {
+      humidity: data.main.humidity,
+      windSpeed: formatFloatPrecision(data.wind.speed),
+    },
+    unitType: unit === 'celsius' ? '°C' : '°F',
+  };
+  return weatherData;
 }
 
 export const weatherApiService = {
