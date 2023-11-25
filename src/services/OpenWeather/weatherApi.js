33934ad3542abcc80d25eeb7430f15b80f54c277
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-11-25 15:00:41 -0300

    refactor: format service response data in format-utils

--- src/services/OpenWeather/weatherApi.js
+++ src/services/OpenWeather/weatherApi.js
@@ -1,63 +1,13 @@
 import api, { API_KEYS } from './api';
-import { formatFloatPrecision, formatDate } from '../../utils/format-utils';
 
 async function getCurrentWeatherByCity(city, unit) {
   const unitType = unit === 'celsius' ? 'metric' : 'imperial';
-  const response = await api.get(`/weather?q=${city}&appid=${API_KEYS.OPENWEATHER}&units=${unitType}`);
-
-  const { data } = response;
-  const weatherData = {
-    name: data.name,
-    condition: data.weather[0].main,
-    icon: data.weather[0].icon,
-    coord: {
-      lat: formatFloatPrecision(data.coord.lat, 2),
-      lon: formatFloatPrecision(data.coord.lon, 2),
-    },
-    temp: {
-      main: formatFloatPrecision(data.main.temp),
-      min: formatFloatPrecision(data.main.temp_min),
-      max: formatFloatPrecision(data.main.temp_max),
-    },
-    details: {
-      humidity: data.main.humidity,
-      windSpeed: formatFloatPrecision(data.wind.speed),
-    },
-    unit: unit,
-    unitType: unit === 'celsius' ? '°C' : '°F',
-  };
-  return weatherData;
+  return await api.get(`/weather?q=${city}&appid=${API_KEYS.OPENWEATHER}&units=${unitType}`);
 }
 
 async function getWeatherForecast(lat, lon, unit) {
   const unitType = unit === 'celsius' ? 'metric' : 'imperial';
-  const response = await api.get(`/forecast/?lat=${lat}&lon=${lon}&appid=${API_KEYS.OPENWEATHER}&units=${unitType}`);
-
-  const { data } = response;
-  const weatherData = data.list.map((data) => {
-    return {
-      temp: formatFloatPrecision(data.main.temp),
-      date: formatDate(data.dt_txt),
-    };
-  });
-
-  const days = {};
-  for (let i = 0; i < weatherData.length; i++) {
-    const date = weatherData[i].date;
-    if (!days[date]) {
-      days[date] = date;
-    }
-  }
-
-  const sortedTemperatures = data.list.sort((a, b) => b.main.temp - a.main.temp);
-  const forecastData = {
-    weatherData,
-    days,
-    maxTemp: sortedTemperatures[0].main.temp,
-    minTemp: sortedTemperatures[weatherData.length - 1].main.temp,
-  };
-
-  return forecastData;
+  return await api.get(`/forecast/?lat=${lat}&lon=${lon}&appid=${API_KEYS.OPENWEATHER}&units=${unitType}`);
 }
 
 export const weatherApiService = {
--- src/utils/format-utils.js
+++ src/utils/format-utils.js
@@ -7,7 +7,7 @@ dayjs.locale('pt-br');
 
 export function formatFloatPrecision(float, precision = 0) {
   const format = float.toFixed(precision);
-  return format;
+  return Number(format);
 }
 
 export function formatTemperature(temp, unit) {
@@ -23,4 +23,54 @@ export function formatDate(date) {
   const weekday = localeData.weekdaysShort(dayjs(date));
   const format = `${day} (${weekday})`;
   return format;
+}
+
+export function formatWeatherData(data, unit) {
+  const weatherData = {
+    name: data.name,
+    condition: data.weather[0].main,
+    icon: data.weather[0].icon,
+    coord: {
+      lat: formatFloatPrecision(data.coord.lat, 2),
+      lon: formatFloatPrecision(data.coord.lon, 2),
+    },
+    temp: {
+      main: formatFloatPrecision(data.main.temp),
+      min: formatFloatPrecision(data.main.temp_min),
+      max: formatFloatPrecision(data.main.temp_max),
+    },
+    details: {
+      humidity: data.main.humidity,
+      windSpeed: formatFloatPrecision(data.wind.speed),
+    },
+    unit: unit,
+    unitType: unit === 'celsius' ? '°C' : '°F',
+  };
+  return weatherData;
+}
+
+export function formatForecastData(data) {
+  const weatherData = data.list.map((data) => {
+    return {
+      temp: formatFloatPrecision(data.main.temp),
+      date: formatDate(data.dt_txt),
+    };
+  });
+
+  const days = {};
+  for (let i = 0; i < weatherData.length; i++) {
+    const date = weatherData[i].date;
+    if (!days[date]) {
+      days[date] = date;
+    }
+  }
+
+  const sortedTemperatures = data.list.sort((a, b) => b.main.temp - a.main.temp);
+  const forecastData = {
+    weatherData,
+    days,
+    maxTemp: formatFloatPrecision(sortedTemperatures[0].main.temp),
+    minTemp: formatFloatPrecision(sortedTemperatures[weatherData.length - 1].main.temp),
+  };
+  return forecastData;
 }
